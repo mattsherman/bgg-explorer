0c1567ece4d3e029ed0af5b85530e24939159a7b
Matt Sherman <matt@jam123.com>
2025-01-20 11:15:46 -0500

    use MainSidebar for mobile home page (temporary until proper home page is implemented)

--- BggReactApp/src/pages/AppHome/index.tsx
+++ BggReactApp/src/pages/AppHome/index.tsx
@@ -1,32 +1,12 @@
-import { NavLink } from 'react-router';
+import { MainSidebar } from '../../components/MainSidebar';
 
 import classes from './index.module.css';
 
 export function AppHome() {
   return (
     <div className={classes['app-home']}>
-      <div className={classes['main-menu']}>
-        <header>
-          <h1>
-            <NavLink to="/">BGG Explorer</NavLink>
-          </h1>
-        </header>
-        <menu>
-          <li>
-            <NavLink to="/hot-items">Hot Items</NavLink>
-          </li>
-          <li>
-            User Collections
-            <menu>
-              <li>
-                <NavLink to="/user/mattsherman/collection">JAM</NavLink>
-              </li>
-              <li>
-                <NavLink to="/user/dccircuit/collection">Andy</NavLink>
-              </li>
-            </menu>
-          </li>
-        </menu>
+      <div className={classes['main-menu-wrapper']}>
+        <MainSidebar />
       </div>
     </div>
   );
